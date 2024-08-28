50732d37be4b4a821688abc9c152b9906950ae7a
LAPTOP-01 <LAPTOP-01@DESKTOP-CVPOHHN>
2024-08-28 17:51:52 +0900

    토큰 입력 허용

---
+++ CrawlingBot/src/main/java/crawlingbot/Application.java
@@ -0,0 +1,12 @@
+package crawlingbot;
+
+import crawlingbot.discord.DiscordBot;
+
+public class Application {
+
+	public static void main(String[] args) {
+		DiscordBot discordBot = new DiscordBot();
+		discordBot.buildingBot();
+	}
+
+}
